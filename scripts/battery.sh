5be1ac17ab0a603ad45a43a6c32c60a6355d73f0
Bernardo Amorim <contato@bamorim.com>
2015-05-21 23:09:33 -0300

    Battery monitoring working

--- scripts/battery.sh
+++ scripts/battery.sh
@@ -3,6 +3,8 @@
 BAT=BAT1
 BATPATH=/sys/class/power_supply/$BAT
 
+notify-send -t 30 -i battery-caution -u low "Battery Monitoring on $BAT is up"
+
 LAST=100
 while true
 do
@@ -10,12 +12,17 @@ do
 
   # Alert on less than 15
   if [ $LAST -gt $CAPACITY ] && [ $CAPACITY -le 15 ]; then
-    export DISPLAY=":1"
-    notify-send -t 30 -u critical "Battery Low!" "Battery is at $CAPACITY%.\nPlease plug in the charger adapter."
+    export DISPLAY=":0"
+    notify-send -t 30 -i battery-low -u critical "Battery Low!" "Battery is at $CAPACITY%.\nPlease plug in the charger adapter."
+  fi
+
+  # Alert for suspending on 3%
+  if [ $CAPACITY -le 3 ]; then
+    notify-send -t 30 -i battery-empty -u critical "Battery is EXTREMELY LOW." "Computer will suspend soon. Find the charger now, dude!"
   fi
 
   # Suspend on 2%
-  if [ $1 -le 2 ]; then
+  if [ $CAPACITY -le 2 ]; then
     systemctl suspend
   fi
   LAST=$CAPACITY
