5a55b56ea52940de0795fccd7f16b19d836e488c
Bernardo Amorim <contato@bamorim.com>
2015-06-07 16:49:56 -0300

    Changed battery script so it always check if battery is decreasing This prevents system to always suspend when low battery, even if it's charging

--- scripts/battery.sh
+++ scripts/battery.sh
@@ -11,19 +11,21 @@ do
   CAPACITY=`cat $BATPATH/capacity`
 
   # Alert on less than 15
-  if [ $LAST -gt $CAPACITY ] && [ $CAPACITY -le 15 ]; then
-    export DISPLAY=":0"
-    notify-send -t 30 -i battery-low -u critical "Battery Low!" "Battery is at $CAPACITY%.\nPlease plug in the charger adapter."
-  fi
+  if [ $LAST -gt $CAPACITY ]; then
+    if [ $CAPACITY -le 15 ]; then
+      export DISPLAY=":0"
+      notify-send -t 30 -i battery-low -u critical "Battery Low!" "Battery is at $CAPACITY%.\nPlease plug in the charger adapter."
+    fi
 
-  # Alert for suspending on 3%
-  if [ $CAPACITY -le 3 ]; then
-    notify-send -t 30 -i battery-empty -u critical "Battery is EXTREMELY LOW." "Computer will suspend soon. Find the charger now, dude!"
-  fi
+    # Alert for suspending on 3%
+    if [ $CAPACITY -le 3 ]; then
+      notify-send -t 30 -i battery-empty -u critical "Battery is EXTREMELY LOW." "Computer will suspend soon. Find the charger now, dude!"
+    fi
 
-  # Suspend on 2%
-  if [ $CAPACITY -le 2 ]; then
-    systemctl suspend
+    # Suspend on 2%
+    if [ $CAPACITY -le 2 ]; then
+      systemctl suspend
+    fi
   fi
   LAST=$CAPACITY
   sleep 5
