7b4665aaa5ec7458b84a39c74520b14f058aeeaa
Bernardo Amorim <contato@bamorim.com>
2022-01-16 18:36:08 +0000

    remove crd2pulumi and fix pulumi login to use actual bash

--- run_once_install-crd2pulumi.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env sh
-
-cd $(mktemp)
-curl -SL https://github.com/pulumi/crd2pulumi/releases/download/v1.0.5/crd2pulumi-v1.0.5-linux-amd64.tar.gz -o crd2pulumi.tar.gz
-tar -xzvf crd2pulumi.tar.gz
-chmod +x crd2pulumi
-mv crd2pulumi $HOME/bin/crd2pulumi
