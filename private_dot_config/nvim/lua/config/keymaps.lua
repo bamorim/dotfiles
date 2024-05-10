ed007a8a99d4eeb1fa89f408c3fbeda4e941b90f
Bernardo Amorim <contato@bamorim.com>
2024-05-10 19:35:41 +0100

    CopyRelPath nvim command

--- private_dot_config/nvim/lua/config/keymaps.lua
+++ private_dot_config/nvim/lua/config/keymaps.lua
@@ -2,3 +2,9 @@
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
 vim.keymap.set("n", "<C-n>", "<cmd>Neotree filesystem toggle left<cr>")
+vim.api.nvim_create_user_command("CopyRelPath", function()
+  local path = vim.fn.fnamemodify(vim.fn.expand("%"), ":.")
+  vim.api.nvim_call_function("setreg", { "+", path })
+  vim.print("Copied path: " .. path)
+end, {})
+vim.keymap.set("n", "<leader>bc", "<cmd>CopyRelPath<cr>", { desc = "Copy relative path of current buffer" })
